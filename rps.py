fb2fcab45d5d7d6de781f753a16f13db7344daab
Travis <tl1802@nyu.edu>
2024-11-27 23:51:23 -0500

    added function and loop

--- rps.py
+++ rps.py
@@ -2,8 +2,7 @@ import random
 
 VALID_CHOICES = ["rock", "paper", "scissors"]
 
-def prompt(message):
-    print(f"==> {message}")
+
 
 def display_winner(player, computer):
     prompt(f"You chose {player}, computer chose {computer}")
@@ -18,6 +17,9 @@ def display_winner(player, computer):
         prompt("Computer wins!")
     else:
         prompt("It's a tie!")
+
+def prompt(message):
+    print(f"==> {message}")
 
 while True:
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
@@ -42,3 +44,20 @@ while True:
 
     if answer[0] == 'n':
         break
+
+# 1. it should not work - can't call prompt function because it hasn't been defined yet
+# you get a NameError
+# It actually does work - don't remember where, but functions are hoisted? 
+# or maybe the functions coexist in memory, the order they're defined doesn't matter
+# they exist simultanexouly and can be called at anytime so long as they are defined
+# before they are called
+# all functions have to be defined in the top of the program in any order
+
+# 2. Since the name of the function is display_winner, it should only display, and not return
+# and this is true. If it were to return a string instead, all we have to do is capture the
+# return value in a variable. Then we can print that variable later.
+
+# 3. One thing we can do use to set the outer while loop to a variable. We can create a variable
+# and set it to a truthy value, for example 1. It runs while truthy. For the case where we want
+# the function to stop, we can reassign the variable to a falsy value such as 0. This will stop 
+# the program.
