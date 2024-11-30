bfa09db1fb0cf13fa7159cdc918fa1eb6674795d
Travis <tl1802@nyu.edu>
2024-11-30 16:50:19 -0500

    works, but needs retouch

--- rps.py
+++ rps.py
@@ -1,37 +1,79 @@
 import random
 
-VALID_CHOICES = ["rock", "paper", "scissors"]
+VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
+SHORT_CHOICES = {"r" : "rock", "p" : "paper", "s" : "scissors", "l" : "lizard", "k" : "spock"}
 
+WINNING_CHOICES = {"rock" : ["scissors", "lizard"],
+                    "paper" : ["rock", "spock"],
+                    "scissors" : ["paper", "lizard"],
+                    "spock" : ["scissors", "rock"],
+                    "lizard" : ["paper", "spock"]}
 
+LOSING_CHOICES = {"rock" : ["paper", "spock"],
+                    "paper" : ["lizard", "scissors"],
+                    "scissors" : ["rock", "spock"],
+                    "spock" : ["paper", "lizard"],
+                    "lizard" : ["scissors", "rock"]}
 
-def display_winner(player, computer):
-    prompt(f"You chose {player}, computer chose {computer}")
-
-    if ((player == "rock" and computer == "scissors") or
-        (player == "paper" and computer == "rock") or
-        (player == "scissors" and computer == "paper")):
-        prompt("You win!")
-    elif ((player == "rock" and computer == "paper") or
-          (player == "paper" and computer == "scissors") or
-          (player == "scissors" and computer == "rock")):
-        prompt("Computer wins!")
-    else:
-        prompt("It's a tie!")
+player_wins = 0
+computer_wins = 0
 
 def prompt(message):
     print(f"==> {message}")
 
+def short_input_mapping(selection):
+    if selection in SHORT_CHOICES:
+        return SHORT_CHOICES[selection]
+    else:
+        return selection
+
+def display_winner(player, computer):
+    prompt(f"You chose {player}, computer chose {computer}")
+
+    if computer in WINNING_CHOICES[player]:
+        prompt("You win!")
+    elif computer in LOSING_CHOICES[player]:
+        prompt("Computer wins!")
+    else:
+        prompt("It's a tie!")
+
+def keep_score(player, computer):
+    if computer in WINNING_CHOICES[player]:
+        global player_wins
+        player_wins += + 1
+        prompt(f'You win this round! ({player_wins} win - first to 3 wins)')
+    elif computer in LOSING_CHOICES[player]:
+        global computer_wins
+        computer_wins += 1
+        prompt(f'Computer wins this round! ({computer_wins} win - first to 3 wins)')
+
+def declare_winner():
+    if player_wins > computer_wins:
+        prompt(f'You Win! {player_wins} win vs Computer\'s {computer_wins} win')
+    else:
+        prompt(f'Computer Wins! {computer_wins} win vs your {player_wins} win')
+
 while True:
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
-    choice = input()
+    prompt(f'You can also type {", ".join(SHORT_CHOICES.keys())}')
+    player_choice = input().lower()
+    player_choice = short_input_mapping(player_choice)
 
-    while choice not in VALID_CHOICES:
+    while player_choice not in VALID_CHOICES:
         prompt("That's not a valid choice")
-        choice = input()
+        prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
+        prompt(f'You can also type {", ".join(SHORT_CHOICES.keys())}')
+        player_choice = input().lower()
+        player_choice = short_input_mapping(player_choice)
 
     computer_choice = random.choice(VALID_CHOICES)
 
-    display_winner(choice, computer_choice)
+    display_winner(player_choice, computer_choice)
+
+    keep_score(player_choice, computer_choice)
+
+    if player_wins == 3 or computer_wins == 3:
+        break
 
     while True:
         prompt("Do you want to play again (y/n)?")
@@ -45,19 +87,4 @@ while True:
     if answer[0] == 'n':
         break
 
-# 1. it should not work - can't call prompt function because it hasn't been defined yet
-# you get a NameError
-# It actually does work - don't remember where, but functions are hoisted? 
-# or maybe the functions coexist in memory, the order they're defined doesn't matter
-# they exist simultanexouly and can be called at anytime so long as they are defined
-# before they are called
-# all functions have to be defined in the top of the program in any order
-
-# 2. Since the name of the function is display_winner, it should only display, and not return
-# and this is true. If it were to return a string instead, all we have to do is capture the
-# return value in a variable. Then we can print that variable later.
-
-# 3. One thing we can do use to set the outer while loop to a variable. We can create a variable
-# and set it to a truthy value, for example 1. It runs while truthy. For the case where we want
-# the function to stop, we can reassign the variable to a falsy value such as 0. This will stop 
-# the program.
+declare_winner()
