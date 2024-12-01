8d9ab1b43d05a7d867f68bac7f14239b2e761de6
Travis <tl1802@nyu.edu>
2024-12-01 16:43:49 -0500

    finished project

--- rps.py
+++ rps.py
@@ -1,118 +1,95 @@
 import random
 
-VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
+VALID_CHOICES = ["rock", "paper", "scissors","lizard", "spock"]
 SHORT_CHOICES = {"r" : "rock", "p" : "paper", "s" : "scissors", "l" : "lizard", "k" : "spock"}
 
-WINNING_CHOICES = {"rock" : ["scissors", "lizard"],
+WINNING_COMBOS = {"rock" : ["scissors", "lizard"],
                     "paper" : ["rock", "spock"],
                     "scissors" : ["paper", "lizard"],
                     "spock" : ["scissors", "rock"],
                     "lizard" : ["paper", "spock"]}
 
 WIN_CONDITION = 3
-player_wins = 0
-computer_wins = 0
 
 def prompt(message):
-
+    """adds arrow to each message"""
     print(f"==> {message}")
 
 def short_input_mapping(selection):
-
+    """maps short one char inputs to choices"""
     if selection in SHORT_CHOICES:
         return SHORT_CHOICES[selection]
 
     return selection
 
-def display_winner(player, computer):
-
-    prompt(f"You chose {player}, computer chose {computer}")
-
-    if computer in WINNING_CHOICES[player]:
-        prompt("You win!")
+def display_round_winner(player, computer, win_counter):
+    """checks WINNING_COMBOS to see who won and displays score""" 
+    if computer in WINNING_COMBOS[player]:
+        prompt(f'You win this round! ({win_counter[0]} win to {win_counter[1]} wins)')
     elif player == computer:
-        prompt("It's a tie!")
+        prompt(f"It's a tie! ({win_counter[0]} win to {win_counter[1]} wins)")
     else:
-        prompt("Computer wins!")
+        prompt(f'Computer wins this round! ({win_counter[0]} win to {win_counter[1]} wins)')
 
-def keep_score(player, computer):
-
-    global player_wins
-    global computer_wins
-    if computer in WINNING_CHOICES[player]:
-        player_wins += 1
-        prompt(f'You win this round! ({player_wins} win - first to 3 wins)')
+def keep_score(player, computer, win_counter):
+    """checks WINNING_COMBOS and updates the scoreboard"""
+    if computer in WINNING_COMBOS[player]:
+        return [win_counter[0] + 1, win_counter[1]]
     elif player == computer:
-        return
+        return win_counter
     else:
-        computer_wins += 1
-        prompt(f'Computer wins this round! ({computer_wins} win - first to 3 wins)')
+        return [win_counter[0], win_counter[1] + 1]
 
-def declare_winner():
-
-    if player_wins > computer_wins:
-        prompt(f'You Win! {player_wins} win vs Computer\'s {computer_wins} win')
-    elif computer_wins > player_wins:
-        prompt(f'Computer Wins! {computer_wins} win vs your {player_wins} win')
+def declare_grand_winner(win_counter):
+    """declare grand winner by comparing scores"""
+    if win_counter[0] > win_counter[1]:
+        prompt(f'You Win! {win_counter[0]} win vs Computer\'s {win_counter[1]} win')
+    elif win_counter[1] > win_counter[0]:
+        prompt(f'Computer Wins! {win_counter[1]} win vs your {win_counter[0]} win')
     else:
-        prompt(f'It\'s a tie! {computer_wins} win vs your {player_wins} win')
+        prompt(f'It\'s a tie! {win_counter[1]} win vs your {win_counter[0]} win')
 
 def player_turn():
-
+    """gets the player's choice"""
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
     prompt(f'You can also type: {", ".join(SHORT_CHOICES.keys())}')
     player_choice = input().lower()
     player_choice = short_input_mapping(player_choice)
     return player_choice
 
-while True:
+def main ():
+    """executes main function"""
+    win_counter = [0, 0]
 
-    player_choice = player_turn()
+    while True:
 
-    while player_choice not in VALID_CHOICES:
-        prompt("That's not a valid choice")
         player_choice = player_turn()
 
-    computer_choice = random.choice(VALID_CHOICES)
+        while player_choice not in VALID_CHOICES:
+            prompt("That's not a valid choice")
+            player_choice = player_turn()
 
-    display_winner(player_choice, computer_choice)
+        computer_choice = random.choice(VALID_CHOICES)
+        prompt(f"You chose {player_choice}, Computer chose {computer_choice}")
 
-    keep_score(player_choice, computer_choice)
+        win_counter = keep_score(player_choice, computer_choice, win_counter)
 
-    if player_wins == WIN_CONDITION or computer_wins == WIN_CONDITION:
-        break
+        display_round_winner(player_choice, computer_choice, win_counter)
 
-    prompt("Do you want to play again (y/n)?")
-    answer = input().lower()
+        if WIN_CONDITION in win_counter:
+            declare_grand_winner(win_counter)
+            break
 
-    while answer not in ['n', 'y']:
-        prompt("That's not a valid choice")
         prompt("Do you want to play again (y/n)?")
         answer = input().lower()
-    
-    if answer == 'n':
-        break
 
-declare_winner()
+        while answer not in ['n', 'y']:
+            prompt("That's not a valid choice")
+            prompt("Do you want to play again (y/n)?")
+            answer = input().lower()
 
+        if answer == 'n':
+            declare_grand_winner(win_counter)
+            break
 
-# def main ():
-    # get player input
-    # get cpu input
-    # set increment counter, win condition
-    # compare player to cpu
-    # add increment to counter
-    # display round result
-    # ask to repeat
-    # loop over again until 3 wins
-    # declare final winner
-    # player_wins = 0
-    # computer_wins = 0
-    
-    # player_choice = player_turn()
-
-    # while player_choice not in VALID_CHOICES:
-    #     prompt("That's not a valid choice")
-    #     player_choice = player_turn()
-
-    # computer_choice = random.choice(VALID_CHOICES)
+main()
